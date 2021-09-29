0b1bddf35ef3ef3003b0244b2f05e2c421290785
Clement-Moreau-Info <88057976+Clement-Moreau-Info@users.noreply.github.com>
2021-09-29 15:07:08 +0200

    Update fth.py

--- fth.py
+++ fth.py
@@ -14,34 +14,89 @@ from typing import Callable, TypeVar
 interval_step = 5
 
 
+######################
+
+##
+# Extraction of all semantic-temporal sequences from a file .csv
+# path : path of the file
+# sep  : separator
+# id   : id sequence colonne
+##
+def extract_temp_seq(path: str, sep=";", id="id") -> List[Temporal_seq]:
+    df = pd.read_csv(path, sep=sep)
+    max_seq = max(df[id]) + 1
+
+    # Activity seq
+    seq_act = [[str(x) for x in df[df[id] == i].iloc[:, 1].values.tolist()]
+                for i in range(1, max_seq)]
+    # Temporal seq
+    seq_temp = [[t for t in df[df[id] == i].iloc[:, 2].values.tolist()]
+                for i in range(1, max_seq)]
+
+    T_max = np.sum(seq_temp[0])
+    
+    # Verify if for all seq_temp, sum(seq_temp) = T_max
+    for i in range(max_seq - 1):
+        if np.sum(seq_temp[i]) != T_max:
+            raise NameError("Séquences ", i, " de longueur différente")
+
+    return [Temporal_seq(seq_act[i], seq_temp[i]) for i in range(max_seq - 1)]
+
+##
+# Fuzzy context function 
+# e     : Edit operation
+# beta  : Boundary in min
+##
 def fuzzy_context(e: Edit, beta=480) -> List[float]:
     return fuzz.trapmf(np.arange(0, np.sum(e.S_i.times), interval_step),
                      [e.t_edit - beta, e.t_edit, e.t_edit + e.delta, e.t_edit + e.delta + beta])
 
-
+##
+# Similarity over the sequence at time t
+# e     : Edit operation
+# t     : Time t
+# sim   : Similarity function between symbols
+##
 def sim_e(e: Edit, t: float, sim: Callable[[str, str], float]) -> float:
     for i in prange(len(e.S_i.times)):
         if np.sum(e.S_i.times[:i]) <= t < np.sum(e.S_i.times[:i + 1]):
             return sim(e.S_i.acts[i], e.x)
     return 0
 
-
+##
+# Gamma cost function
+# e     : Edit operation
+# sim   : Similarity function between symbols
+# beta  : Boundary in min
+##
 def cost_gamma(e: Edit, sim: Callable[[str, str], float], beta: float) -> float:
-    I = np.arange(0, np.sum(e.S_i.times), interval_step)
-    mu = fuzz.trapmf(I, [e.t_edit - beta, e.t_edit, e.t_edit + e.delta,
+    I = np.arange(0, np.sum(e.S_i.times), interval_step)                           # Time interval
+    mu = fuzz.trapmf(I, [e.t_edit - beta, e.t_edit, e.t_edit + e.delta,            # Fuzzy function
                        e.t_edit + e.delta + beta])
-    sim_fun = [sim_e(e, t, sim) for t in I]
-    sim_context = [mu[i] * sim_fun[i] for i in range(len(I))]
-    tab_gate = np.arange(0, e.delta, interval_step)
-    gate = fuzz.trapmf(tab_gate, [0, 0, e.delta, e.delta])
-    convo = signal.convolve(sim_context, gate, mode='same') / (e.delta / interval_step)
+    sim_fun = [sim_e(e, t, sim) for t in I]                                        # Similarity over the sequence
+    sim_context = [mu[i] * sim_fun[i] for i in range(len(I))]                      # Merge of fuzzy function and similarity
+    tab_gate = np.arange(0, e.delta, interval_step)                                # Interval of gate function 
+    gate = fuzz.trapmf(tab_gate, [0, 0, e.delta, e.delta])                         # Encoding as fuzzy function 
+    convo = signal.convolve(sim_context, gate, mode='same') / (e.delta / interval_step)  # Convolution product of gate and merged fuzzy-sim function 
     return 1 - np.max(convo)
 
-
+##
+# Delta cost function
+# e     : Edit operation
+# sim   : Similarity function between symbols
+# beta  : Boundary in min
+##
 def cost_delta(e: Edit, sim: Callable[[str, str], float], beta: float) -> float:
     return e.delta * cost_gamma(e, sim, beta)
 
-
+##
+# One sided FTH
+# S1    : Semantic-temporal seq 1
+# S2    : Semantic-temporal seq 1
+# sim   : Similarity function between symbols
+# beta  : Boundary in min
+# f     : Cost function (choose `cost_delta` or `cost_gamma`
+##
 def one_sided_fth(S1: Temporal_seq, S2: Temporal_seq, sim: Callable[[str, str], float], beta: float, f=cost_delta) -> float:
     sum_cost = 0
     for i in prange(len(S1.acts)):
@@ -49,7 +104,14 @@ def one_sided_fth(S1: Temporal_seq, S2: Temporal_seq, sim: Callable[[str, str], 
         sum_cost += f(e, sim, beta)
     return sum_cost
 
-
+##
+# FTH
+# S1    : Semantic-temporal seq 1
+# S2    : Semantic-temporal seq 1
+# sim   : Similarity function between symbols
+# beta  : Boundary in min
+# f     : Cost function (choose `cost_delta` or `cost_gamma`
+##
 def fth(S1: Temporal_seq, S2: Temporal_seq, sim: Callable[[str, str], float], beta: float, f=cost_delta) -> float:
     return max(one_sided_fth(S1, S2, sim, beta, f),
                one_sided_fth(S2, S1, sim, beta, f))
