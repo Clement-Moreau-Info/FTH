4b337955e32a76fb3a31b50f9d2b74d0af055a33
Clement-Moreau-Info <88057976+Clement-Moreau-Info@users.noreply.github.com>
2021-10-28 12:17:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fth import *
 import networkx as nx
-from functools import lru_cache
+from functools import lru_cache, partial
+import pandas as pd
 
 # Path to ontology
 path_onto = "ontology_emd.txt"
@@ -10,48 +11,38 @@ path_onto = "ontology_emd.txt"
 
 @lru_cache(maxsize=100000)
 def wu_palmer(x: str, y: str, path: str, rootnode="All") -> float:
-    ontologie = nx.read_adjlist(path, create_using=nx.DiGraph)
-    return (2.0 * nx.shortest_path_length(ontologie, rootnode, nx.lowest_common_ancestor(ontologie, x, y))) / (
-            nx.shortest_path_length(ontologie, rootnode, x) + nx.shortest_path_length(ontologie, rootnode, y))
+    ontology = nx.read_adjlist(path, create_using=nx.DiGraph)
+    return (2.0 * nx.shortest_path_length(ontology, rootnode, nx.lowest_common_ancestor(ontology, x, y))) / (
+            nx.shortest_path_length(ontology, rootnode, x) + nx.shortest_path_length(ontology, rootnode, y))
 
 
-# Define the similarity used
-def sim(x: str, y: str) -> float:
-    return wu_palmer(x, y, path_onto)
-
-
-##
-# Extraction of all semantic-temporal sequences from a file .csv
-# path : path of the file
-# sep  : separator
-# id   : id sequence colonne
-##
-def extract_temp_seq(path: str, sep=";", id="id") -> List[Temporal_seq]:
+def extract_temp_seq(path: str, sep=";", id="id") -> List[TemporalSeq]:
     df = pd.read_csv(path, sep=sep)
     max_seq = max(df[id]) + 1
 
-    # Activity seq
     seq_act = [[str(x) for x in df[df[id] == i].iloc[:, 1].values.tolist()]
                 for i in range(1, max_seq)]
-    # Temporal seq
     seq_temp = [[t for t in df[df[id] == i].iloc[:, 2].values.tolist()]
                 for i in range(1, max_seq)]
 
     T_max = np.sum(seq_temp[0])
-    
-    # Verify if for all seq_temp, sum(seq_temp) = T_max
     for i in range(max_seq - 1):
         if np.sum(seq_temp[i]) != T_max:
-            raise NameError("Séquences ", i, " de longueur différente")
+            raise NameError("Seq ", i, " has a different temporal size")
 
-    return [Temporal_seq(seq_act[i], seq_temp[i]) for i in range(max_seq - 1)]
+    return [TemporalSeq(seq_act[i], seq_temp[i]) for i in range(max_seq - 1)]
+
+
+def main():
+    seq_alice = TemporalSeq(['1', '133', '100', '11', '100', '51', '131', '12', '1'],
+                             [210, 20, 10, 250, 15, 60, 15, 290, 570])
+    seq_bob = TemporalSeq(['1', '100', '133', '11', '100', '1'],
+                           [230, 10, 30, 480, 60, 630])
+
+    print("FTH(S_alice, S_bob) = ", fth(seq_bob, seq_alice, partial(wu_palmer, path=path_onto), 240, cost_delta))
+
+    T_seq = extract_temp_seq("test_seq.csv")
 
 
 if __name__ == '__main__':
-    S_alice = Temporal_seq(['1', '133', '100', '11', '100', '51', '131', '12', '1'],
-                           [210, 20, 10, 250, 15, 60, 15, 290, 570])
-
-    S_bob = Temporal_seq(['1', '100', '133', '11', '100', '1'],
-                         [230, 10, 30, 480, 60, 630])
-
-    print("FTH(S_alice, S_bob) = ", fth(S_bob, S_alice, sim, 240, cost_delta))
+    main()
