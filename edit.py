c93624667d94ace64dca4a7b2e41244b99fb4d1e
Clement-Moreau-Info <88057976+Clement-Moreau-Info@users.noreply.github.com>
2021-10-28 12:15:07 +0200

    Update edit.py

--- edit.py
+++ edit.py
@@ -1,11 +1,8 @@
 from temporal_seq import *
-from typing import TypeVar
-
-T = TypeVar('T')
 
 
 class Edit:
-    def __init__(self, x: T, delta: float, t_edit: float, seq_i: Temporal_seq):
+    def __init__(self, x: T, delta: float, t_edit: float, seq_i: TemporalSeq):
         self.x = x
         self.delta = delta
         self.t_edit = t_edit
