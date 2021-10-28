e616d842c32849786ea99840170f4c02af903ba3
Clement-Moreau-Info <88057976+Clement-Moreau-Info@users.noreply.github.com>
2021-10-28 12:11:14 +0200

    Update fth.py

--- fth.py
+++ fth.py
@@ -3,18 +3,15 @@ from edit import *
 import numpy as np
 from temporal_seq import *
 from scipy import signal
-import networkx as nx
-from functools import lru_cache
 from numba import prange
-from typing import Callable, TypeVar
+from typing import Callable
 
-######################
-## GLOBAL VARIABLES ##
-######################
+###
+# GLOBAL VARIABLES
+###
 interval_step = 5
 
-
-######################
+#####
 
 
 ##
@@ -23,8 +20,8 @@ interval_step = 5
 # beta  : Boundary in min
 ##
 def fuzzy_context(e: Edit, beta=480) -> List[float]:
-    return fuzz.trapmf(np.arange(0, np.sum(e.S_i.times), interval_step),
-                     [e.t_edit - beta, e.t_edit, e.t_edit + e.delta, e.t_edit + e.delta + beta])
+    return fuzz.trapmf(np.arange(0, np.sum(e.seq_i.times), interval_step),
+                       [e.t_edit - beta, e.t_edit, e.t_edit + e.delta, e.t_edit + e.delta + beta])
 
 ##
 # Similarity over the sequence at time t
@@ -32,10 +29,10 @@ def fuzzy_context(e: Edit, beta=480) -> List[float]:
 # t     : Time t
 # sim   : Similarity function between symbols
 ##
-def sim_e(e: Edit, t: float, sim: Callable[[str, str], float]) -> float:
-    for i in prange(len(e.S_i.times)):
-        if np.sum(e.S_i.times[:i]) <= t < np.sum(e.S_i.times[:i + 1]):
-            return sim(e.S_i.acts[i], e.x)
+def sim_e(e: Edit, t: float, sim: Callable[[T, T], float]) -> float:
+    for i in prange(len(e.seq_i.times)):
+        if np.sum(e.seq_i.times[:i]) <= t < np.sum(e.seq_i.times[:i + 1]):
+            return sim(e.seq_i.acts[i], e.x)
     return 0
 
 ##
@@ -44,15 +41,15 @@ def sim_e(e: Edit, t: float, sim: Callable[[str, str], float]) -> float:
 # sim   : Similarity function between symbols
 # beta  : Boundary in min
 ##
-def cost_gamma(e: Edit, sim: Callable[[str, str], float], beta: float) -> float:
-    I = np.arange(0, np.sum(e.S_i.times), interval_step)                           # Time interval
-    mu = fuzz.trapmf(I, [e.t_edit - beta, e.t_edit, e.t_edit + e.delta,            # Fuzzy function
-                       e.t_edit + e.delta + beta])
-    sim_fun = [sim_e(e, t, sim) for t in I]                                        # Similarity over the sequence
-    sim_context = [mu[i] * sim_fun[i] for i in range(len(I))]                      # Merge of fuzzy function and similarity
-    tab_gate = np.arange(0, e.delta, interval_step)                                # Interval of gate function 
-    gate = fuzz.trapmf(tab_gate, [0, 0, e.delta, e.delta])                         # Encoding as fuzzy function 
-    convo = signal.convolve(sim_context, gate, mode='same') / (e.delta / interval_step)  # Convolution product of gate and merged fuzzy-sim function 
+def cost_gamma(e: Edit, sim: Callable[[T, T], float], beta: float) -> float:
+    I = np.arange(0, np.sum(e.seq_i.times), interval_step)                  # Time interval
+    mu = fuzz.trapmf(I, [e.t_edit - beta, e.t_edit, e.t_edit + e.delta,     # Fuzzy function
+                     e.t_edit + e.delta + beta])
+    sim_fun = [sim_e(e, t, sim) for t in I]                                 # Similarity over the sequence
+    sim_context = [mu[i] * sim_fun[i] for i in range(len(I))]               # Merge of fuzzy function and similarity
+    tab_gate = np.arange(0, e.delta, interval_step)                         # Interval of gate function
+    gate = fuzz.trapmf(tab_gate, [0, 0, e.delta, e.delta])                  # Encoding as fuzzy function 
+    convo = signal.convolve(sim_context, gate, mode='same') / (e.delta / interval_step) # Convolution product of gate and merged fuzzy-sim function
     return 1 - np.max(convo)
 
 ##
@@ -61,7 +58,7 @@ def cost_gamma(e: Edit, sim: Callable[[str, str], float], beta: float) -> float:
 # sim   : Similarity function between symbols
 # beta  : Boundary in min
 ##
-def cost_delta(e: Edit, sim: Callable[[str, str], float], beta: float) -> float:
+def cost_delta(e: Edit, sim: Callable[[T, T], float], beta: float) -> float:
     return e.delta * cost_gamma(e, sim, beta)
 
 ##
@@ -72,10 +69,10 @@ def cost_delta(e: Edit, sim: Callable[[str, str], float], beta: float) -> float:
 # beta  : Boundary in min
 # f     : Cost function (choose `cost_delta` or `cost_gamma`
 ##
-def one_sided_fth(S1: Temporal_seq, S2: Temporal_seq, sim: Callable[[str, str], float], beta: float, f=cost_delta) -> float:
+def one_sided_fth(seq1: Temporal_seq, seq2: Temporal_seq, sim: Callable[[T, T], float], beta: float, f=cost_delta) -> float:
     sum_cost = 0
-    for i in prange(len(S1.acts)):
-        e = Edit(S1.acts[i], S1.times[i], np.sum(S1.times[:i]), S2)
+    for i in prange(len(seq1.acts)):
+        e = Edit(seq1.acts[i], seq1.times[i], np.sum(seq1.times[:i]), seq2)
         sum_cost += f(e, sim, beta)
     return sum_cost
 
@@ -87,6 +84,6 @@ def one_sided_fth(S1: Temporal_seq, S2: Temporal_seq, sim: Callable[[str, str], 
 # beta  : Boundary in min
 # f     : Cost function (choose `cost_delta` or `cost_gamma`
 ##
-def fth(S1: Temporal_seq, S2: Temporal_seq, sim: Callable[[str, str], float], beta: float, f=cost_delta) -> float:
-    return max(one_sided_fth(S1, S2, sim, beta, f),
-               one_sided_fth(S2, S1, sim, beta, f))
+def fth(seq1: Temporal_seq, seq2: Temporal_seq, sim: Callable[[T, T], float], beta: float, f=cost_delta) -> float:
+    return max(one_sided_fth(seq1, seq2, sim, beta, f),
+               one_sided_fth(seq2, seq1, sim, beta, f))
