ba5e032024d32e2653b5ebcc52f4ce4ac2f3715a
Clement-Moreau-Info <88057976+Clement-Moreau-Info@users.noreply.github.com>
2021-11-17 15:04:28 +0100

    Update edit.py

--- edit.py
+++ edit.py
@@ -3,6 +3,12 @@ from temporal_seq import *
 
 class Edit:
     def __init__(self, x: T, delta: float, t_edit: float, seq_i: TemporalSeq):
+        """
+        :param x:       Edited symbol
+        :param delta:   Duration of x
+        :param t_edit:  Time of edition in the sequence seq_i
+        :param seq_i:   Edited sequence
+        """
         self.x = x
         self.delta = delta
         self.t_edit = t_edit
