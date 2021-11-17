b6d4316c1d36924c3ed68c65f8587405433b3acc
Clement-Moreau-Info <88057976+Clement-Moreau-Info@users.noreply.github.com>
2021-11-17 16:04:05 +0100

    Update fth.py

--- fth.py
+++ fth.py
@@ -14,34 +14,36 @@ interval_step = 5
 #####
 
 
-##
-# Fuzzy context function 
-# e     : Edit operation
-# beta  : Boundary in min
-##
 def fuzzy_context(e: Edit, beta=480) -> List[float]:
+    """
+    :param e:       Edit operation
+    :param beta:    Boundary in unit_of_time
+    :return:        Fuzzy temporal function
+    """
     return fuzz.trapmf(np.arange(0, np.sum(e.seq_i.times), interval_step),
                        [e.t_edit - beta, e.t_edit, e.t_edit + e.delta, e.t_edit + e.delta + beta])
 
-##
-# Similarity over the sequence at time t
-# e     : Edit operation
-# t     : Time t
-# sim   : Similarity function between symbols
-##
+
 def sim_e(e: Edit, t: float, sim: Callable[[T, T], float]) -> float:
+    """
+    :param e:     Edit operation
+    :param t:     Time t
+    :param sim:   Similarity function between symbols
+    :return:      Similarity over the sequence at time t
+    """
     for i in prange(len(e.seq_i.times)):
         if np.sum(e.seq_i.times[:i]) <= t < np.sum(e.seq_i.times[:i + 1]):
             return sim(e.seq_i.acts[i], e.x)
     return 0
 
-##
-# Gamma cost function
-# e     : Edit operation
-# sim   : Similarity function between symbols
-# beta  : Boundary in min
-##
+
 def cost_gamma(e: Edit, sim: Callable[[T, T], float], beta: float) -> float:
+    """
+    :param e:     Edit operation
+    :param sim:   Similarity function between symbols
+    :param beta:  Boundary in unit_of_time
+    :return:      Cost of the operation e
+    """
     I = np.arange(0, np.sum(e.seq_i.times), interval_step)                  # Time interval
     mu = fuzz.trapmf(I, [e.t_edit - beta, e.t_edit, e.t_edit + e.delta,     # Fuzzy function
                      e.t_edit + e.delta + beta])
@@ -52,38 +54,41 @@ def cost_gamma(e: Edit, sim: Callable[[T, T], float], beta: float) -> float:
     convo = signal.convolve(sim_context, gate, mode='same') / (e.delta / interval_step) # Convolution product of gate and merged fuzzy-sim function
     return 1 - np.max(convo)
 
-##
-# Delta cost function
-# e     : Edit operation
-# sim   : Similarity function between symbols
-# beta  : Boundary in min
-##
+
 def cost_delta(e: Edit, sim: Callable[[T, T], float], beta: float) -> float:
+    """
+    :param e:     Edit operation
+    :param sim:   Similarity function between symbols
+    :param beta:  Boundary in unit_of_time
+    :return:      Cost of the operation e (Hamming version weighted by time)
+    """
     return e.delta * cost_gamma(e, sim, beta)
 
-##
-# One sided FTH
-# S1    : Semantic-temporal seq 1
-# S2    : Semantic-temporal seq 1
-# sim   : Similarity function between symbols
-# beta  : Boundary in min
-# f     : Cost function (choose `cost_delta` or `cost_gamma`
-##
+
 def one_sided_fth(seq1: TemporalSeq, seq2: TemporalSeq, sim: Callable[[T, T], float], beta: float, f=cost_delta) -> float:
+    """
+    :param seq1:    Semantic-temporal seq 1
+    :param seq2:    Semantic-temporal seq 2
+    :param sim:     Similarity function between symbols
+    :param beta:    Boundary in unit_of_time
+    :param f:       Cost function (choose `cost_delta` or `cost_gamma`)
+    :return:        Sum of costs to change seq1 to seq2
+    """
     sum_cost = 0
     for i in prange(len(seq1.acts)):
         e = Edit(seq1.acts[i], seq1.times[i], np.sum(seq1.times[:i]), seq2)
         sum_cost += f(e, sim, beta)
     return sum_cost
 
-##
-# FTH
-# S1    : Semantic-temporal seq 1
-# S2    : Semantic-temporal seq 1
-# sim   : Similarity function between symbols
-# beta  : Boundary in min
-# f     : Cost function (choose `cost_delta` or `cost_gamma`
-##
+
 def fth(seq1: TemporalSeq, seq2: TemporalSeq, sim: Callable[[T, T], float], beta: float, f=cost_delta) -> float:
+    """
+    :param seq1:    Semantic-temporal seq 1
+    :param seq2:    Semantic-temporal seq 2
+    :param sim:     Similarity function between symbols
+    :param beta:    Boundary in unit_of_time
+    :param f:       Cost function (choose `cost_delta` or `cost_gamma`)
+    :return:        FTH(seq1, seq2)
+    """
     return max(one_sided_fth(seq1, seq2, sim, beta, f),
                one_sided_fth(seq2, seq1, sim, beta, f))
